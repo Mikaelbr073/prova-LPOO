06235dead5acf09c09dbd9ea5705326b1da99faa
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2020-12-11 22:15:07 -0300

    Atulizando o diagrama de UML

--- src/entidade/Cliente.java
+++ src/entidade/Cliente.java
@@ -5,10 +5,10 @@ package entidade;
  *
  */
 public class Cliente {
-	
+
 	private String nome;
 	private int cpf;
-	
+
 	public Cliente(String nome, int cpf) {
 		this.nome = nome;
 		this.cpf = cpf;
@@ -18,11 +18,16 @@ public class Cliente {
 		return nome;
 	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public int getCpf() {
 		return cpf;
 	}
 
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
 
-
-	
 }
