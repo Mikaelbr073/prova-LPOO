62c352e229d49485752ad6e2f74a3ad02ac2bffe
Mikael Carvalho <mcb3@discente.ifpe.edu.br>
2020-12-11 21:33:11 -0300

    Projeto criado

---
+++ src/aplicacao/Teste.java
@@ -0,0 +1,23 @@
+package aplicacao;
+
+/**
+ * @author Mikael C. Barros
+ *
+ */
+public class Teste {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(""
+				+ "1. Cadastrar Conta ou Poupança \n"
+				+ "2. Realizar depósito \n"
+				+ "3. Render Juros \n"
+				+ "4. Consultar número e nome da agência \n"
+				+ "5. Alterar o número e nome da agência \n"
+				+ "0. Sair \n"
+				);
+		
+	}
+
+}
---
+++ src/entidade/Banco.java
@@ -0,0 +1,35 @@
+package entidade;
+
+/**
+ * @author Mikael C. Barros
+ *
+ */
+public class Banco {
+	
+	private int numeroAgencia;
+	private String nomeAgencia;
+	
+	public Banco(int numeroAgencia, String nomeAgencia) {
+		this.numeroAgencia = numeroAgencia;
+		this.nomeAgencia = nomeAgencia;
+	}
+
+	public int getNumeroAgencia() {
+		return numeroAgencia;
+	}
+
+	public void setNumeroAgencia(int numeroAgencia) {
+		this.numeroAgencia = numeroAgencia;
+	}
+
+	public String getNomeAgencia() {
+		return nomeAgencia;
+	}
+
+	public void setNomeAgencia(String nomeAgencia) {
+		this.nomeAgencia = nomeAgencia;
+	}
+	
+	
+
+}
